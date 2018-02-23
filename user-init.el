6332c5f4d7ea444a584f15f8544734a95950e043
Anthony Soto <0xsoto@gmail.com>
2018-02-23 09:00:28 -0500

    Initial commit

---
+++ user-init.el
@@ -0,0 +1,28 @@
+;;; Package --- Summary 99.9% of my config file is here.
+;;; Commentary:
+;;; Code:
+
+
+(load "~/.emacs.d/user-init/user-core/user-system")
+(load "~/.emacs.d/user-init/user-core/user-appearance")
+(load "~/.emacs.d/user-init/user-core/user-irc")
+(load "~/.emacs.d/user-init/user-core/user-functions")
+(load "~/.emacs.d/user-init/user-core/user-dired-mode")
+
+
+(load "~/.emacs.d/user-init/user-packages/user-company-mode")
+(load "~/.emacs.d/user-init/user-packages/user-helm-mode")
+(load "~/.emacs.d/user-init/user-packages/user-projectile-mode")
+(load "~/.emacs.d/user-init/user-packages/user-yas-mode")
+(load "~/.emacs.d/user-init/user-packages/user-project-explorer")
+
+(load "~/.emacs.d/user-init/user-lang/user-prog-mode")
+(load "~/.emacs.d/user-init/user-lang/user-c-mode")
+(load "~/.emacs.d/user-init/user-lang/user-elisp-mode")
+(load "~/.emacs.d/user-init/user-lang/user-lua-mode")
+(load "~/.emacs.d/user-init/user-lang/user-nim-mode")
+
+
+
+(provide 'user-init)
+;;; user-init.el ends here
